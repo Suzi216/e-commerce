36e0132a3e2ad05843f9a01c0b4757975c003c31
Admire-Companies <admirecompanies7@gmail.com>
2025-04-20 23:48:36 +0200

    remove the security

--- src/main/java/com/projects/e_commerce_service/controllers/AuthController.java
+++ src/main/java/com/projects/e_commerce_service/controllers/AuthController.java
@@ -1,47 +1,47 @@
-package com.projects.e_commerce_service.controllers;
-
-import com.projects.e_commerce_service.dtos.AuthenticationRequestDto;
-import com.projects.e_commerce_service.dtos.AuthenticationResponseDto;
-import com.projects.e_commerce_service.dtos.UserRegistrationRequestDto;
-import com.projects.e_commerce_service.exceptions.InvalidPayloadException;
-import com.projects.e_commerce_service.exceptions.TokenGenerationException;
-import com.projects.e_commerce_service.services.AuthService;
-import com.projects.e_commerce_service.services.JwtTokenProvider;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/api/authenticate")
-@CrossOrigin(origins = {"http://localhost:8080"})
-@RequiredArgsConstructor
-public class AuthController {
-
-    private final JwtTokenProvider jwtTokenProvider;
-    private final AuthService authService;
-
-
-    @PostMapping()
-    public ResponseEntity<AuthenticationResponseDto> authenticateUser(@RequestBody AuthenticationRequestDto request) throws InvalidPayloadException, TokenGenerationException {
-
-        AuthenticationResponseDto token= authService.loginUser(request);
-
-        return ResponseEntity.ok((token));
-    }
-    @PostMapping(value = "/register-user")
-    public ResponseEntity<AuthenticationResponseDto> registerUser(@RequestBody UserRegistrationRequestDto request ) throws InvalidPayloadException, TokenGenerationException {
-        AuthenticationResponseDto token= authService.registerUser(request);
-
-        return ResponseEntity.ok((token));
-
-    }
-    @PostMapping(value = "/register-super-admin")
-    public ResponseEntity<AuthenticationResponseDto> registerSuperAdmin(@RequestBody UserRegistrationRequestDto request ) throws InvalidPayloadException, TokenGenerationException {
-        AuthenticationResponseDto token= authService.registerSuperAdmin(request);
-
-        return ResponseEntity.ok((token));
-
-    }
-
-}
-
+//package com.projects.e_commerce_service.controllers;
+//
+//import com.projects.e_commerce_service.dtos.AuthenticationRequestDto;
+//import com.projects.e_commerce_service.dtos.AuthenticationResponseDto;
+//import com.projects.e_commerce_service.dtos.UserRegistrationRequestDto;
+//import com.projects.e_commerce_service.exceptions.InvalidPayloadException;
+//import com.projects.e_commerce_service.exceptions.TokenGenerationException;
+//import com.projects.e_commerce_service.services.AuthService;
+//import com.projects.e_commerce_service.services.JwtTokenProvider;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequestMapping("/api/authenticate")
+//@CrossOrigin(origins = {"http://localhost:8080"})
+//@RequiredArgsConstructor
+//public class AuthController {
+//
+//    private final JwtTokenProvider jwtTokenProvider;
+//    private final AuthService authService;
+//
+//
+//    @PostMapping()
+//    public ResponseEntity<AuthenticationResponseDto> authenticateUser(@RequestBody AuthenticationRequestDto request) throws InvalidPayloadException, TokenGenerationException {
+//
+//        AuthenticationResponseDto token= authService.loginUser(request);
+//
+//        return ResponseEntity.ok((token));
+//    }
+//    @PostMapping(value = "/register-user")
+//    public ResponseEntity<AuthenticationResponseDto> registerUser(@RequestBody UserRegistrationRequestDto request ) throws InvalidPayloadException, TokenGenerationException {
+//        AuthenticationResponseDto token= authService.registerUser(request);
+//
+//        return ResponseEntity.ok((token));
+//
+//    }
+//    @PostMapping(value = "/register-super-admin")
+//    public ResponseEntity<AuthenticationResponseDto> registerSuperAdmin(@RequestBody UserRegistrationRequestDto request ) throws InvalidPayloadException, TokenGenerationException {
+//        AuthenticationResponseDto token= authService.registerSuperAdmin(request);
+//
+//        return ResponseEntity.ok((token));
+//
+//    }
+//
+//}
+//
--- src/main/java/com/projects/e_commerce_service/services/AuthService.java
+++ src/main/java/com/projects/e_commerce_service/services/AuthService.java
@@ -1,91 +1,91 @@
-package com.projects.e_commerce_service.services;
-
-import com.projects.e_commerce_service.dtos.AuthenticationRequestDto;
-import com.projects.e_commerce_service.dtos.AuthenticationResponseDto;
-import com.projects.e_commerce_service.dtos.UserRegistrationRequestDto;
-import com.projects.e_commerce_service.entities.RoleEnum;
-import com.projects.e_commerce_service.entities.User;
-import com.projects.e_commerce_service.exceptions.InvalidPayloadException;
-import com.projects.e_commerce_service.exceptions.TokenGenerationException;
-import com.projects.e_commerce_service.repositories.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
-
-@Service
-@RequiredArgsConstructor
-public class AuthService {
-
-    private final UserRepository userRepository;
-    private final JwtTokenProvider jwtTokenProvider;
-    private final PasswordEncoder passwordEncoder;
-    public AuthenticationResponseDto loginUser(@RequestBody AuthenticationRequestDto payload) throws InvalidPayloadException, TokenGenerationException {//        boolean isSamePassword = checkUserPassword(payload.getPassword(), user.getPassword());
-
-        User user = userRepository.findByUsername(payload.getUsername()).orElseThrow(() -> new InvalidPayloadException("User not found"));
-
-        boolean isSamePassword = checkUserPassword(payload.getPassword(), user.getPassword());
-        if (!isSamePassword) {
-            throw new InvalidPayloadException("Wrong password.");
-        }
-        AuthenticationResponseDto authResponseDto = new ModelMapper().map(user, AuthenticationResponseDto.class);
-        try {
-            String accessToken = jwtTokenProvider.generateToken(user);
-            authResponseDto.setJwt(accessToken);
-        } catch (Exception e) {
-            throw new TokenGenerationException();
-        }
-
-        return authResponseDto;
-    }
-
-    private boolean checkUserPassword(String plainPassword, String encodedPassword) {
-//        return true;
-        return passwordEncoder.matches(plainPassword, encodedPassword);
-    }
-
-    public AuthenticationResponseDto registerUser(UserRegistrationRequestDto payload) throws InvalidPayloadException, TokenGenerationException {
-        if (existingUser(payload.getUsername())){
-            throw new InvalidPayloadException("User  found");
-        }
-        ModelMapper modelMapper = new ModelMapper();
-        User user = modelMapper.map(payload, User.class);
-        user.setRole(RoleEnum.USER);
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        userRepository.save(user);
-        AuthenticationResponseDto authResponseDto = new ModelMapper().map(user, AuthenticationResponseDto.class);
-
-        try {
-            String accessToken = jwtTokenProvider.generateToken(user);
-            authResponseDto.setJwt(accessToken);
-        } catch (Exception e) {
-            throw new TokenGenerationException();
-        }
-        return authResponseDto;
-    }
-
-    public boolean existingUser(String email) {
-        return userRepository.findByUsername(email).isPresent();
-    }
-
-    public AuthenticationResponseDto registerSuperAdmin(UserRegistrationRequestDto payload) throws InvalidPayloadException, TokenGenerationException {
-        if (existingUser(payload.getUsername())){
-            throw new InvalidPayloadException("User  found");
-        }
-        ModelMapper modelMapper = new ModelMapper();
-        User user = modelMapper.map(payload, User.class);
-        user.setRole(RoleEnum.SUPER_ADMIN);
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        userRepository.save(user);
-        AuthenticationResponseDto authResponseDto = new ModelMapper().map(user, AuthenticationResponseDto.class);
-
-        try {
-            String accessToken = jwtTokenProvider.generateToken(user);
-            authResponseDto.setJwt(accessToken);
-        } catch (Exception e) {
-            throw new TokenGenerationException();
-        }
-        return authResponseDto;
-    }
-}
+//package com.projects.e_commerce_service.services;
+//
+//import com.projects.e_commerce_service.dtos.AuthenticationRequestDto;
+//import com.projects.e_commerce_service.dtos.AuthenticationResponseDto;
+//import com.projects.e_commerce_service.dtos.UserRegistrationRequestDto;
+//import com.projects.e_commerce_service.entities.RoleEnum;
+//import com.projects.e_commerce_service.entities.User;
+//import com.projects.e_commerce_service.exceptions.InvalidPayloadException;
+//import com.projects.e_commerce_service.exceptions.TokenGenerationException;
+//import com.projects.e_commerce_service.repositories.UserRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.modelmapper.ModelMapper;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.bind.annotation.RequestBody;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class AuthService {
+//
+//    private final UserRepository userRepository;
+//    private final JwtTokenProvider jwtTokenProvider;
+//    private final PasswordEncoder passwordEncoder;
+//    public AuthenticationResponseDto loginUser(@RequestBody AuthenticationRequestDto payload) throws InvalidPayloadException, TokenGenerationException {//        boolean isSamePassword = checkUserPassword(payload.getPassword(), user.getPassword());
+//
+//        User user = userRepository.findByUsername(payload.getUsername()).orElseThrow(() -> new InvalidPayloadException("User not found"));
+//
+//        boolean isSamePassword = checkUserPassword(payload.getPassword(), user.getPassword());
+//        if (!isSamePassword) {
+//            throw new InvalidPayloadException("Wrong password.");
+//        }
+//        AuthenticationResponseDto authResponseDto = new ModelMapper().map(user, AuthenticationResponseDto.class);
+//        try {
+//            String accessToken = jwtTokenProvider.generateToken(user);
+//            authResponseDto.setJwt(accessToken);
+//        } catch (Exception e) {
+//            throw new TokenGenerationException();
+//        }
+//
+//        return authResponseDto;
+//    }
+//
+//    private boolean checkUserPassword(String plainPassword, String encodedPassword) {
+////        return true;
+//        return passwordEncoder.matches(plainPassword, encodedPassword);
+//    }
+//
+//    public AuthenticationResponseDto registerUser(UserRegistrationRequestDto payload) throws InvalidPayloadException, TokenGenerationException {
+//        if (existingUser(payload.getUsername())){
+//            throw new InvalidPayloadException("User  found");
+//        }
+//        ModelMapper modelMapper = new ModelMapper();
+//        User user = modelMapper.map(payload, User.class);
+//        user.setRole(RoleEnum.USER);
+//        user.setPassword(passwordEncoder.encode(user.getPassword()));
+//        userRepository.save(user);
+//        AuthenticationResponseDto authResponseDto = new ModelMapper().map(user, AuthenticationResponseDto.class);
+//
+//        try {
+//            String accessToken = jwtTokenProvider.generateToken(user);
+//            authResponseDto.setJwt(accessToken);
+//        } catch (Exception e) {
+//            throw new TokenGenerationException();
+//        }
+//        return authResponseDto;
+//    }
+//
+//    public boolean existingUser(String email) {
+//        return userRepository.findByUsername(email).isPresent();
+//    }
+//
+//    public AuthenticationResponseDto registerSuperAdmin(UserRegistrationRequestDto payload) throws InvalidPayloadException, TokenGenerationException {
+//        if (existingUser(payload.getUsername())){
+//            throw new InvalidPayloadException("User  found");
+//        }
+//        ModelMapper modelMapper = new ModelMapper();
+//        User user = modelMapper.map(payload, User.class);
+//        user.setRole(RoleEnum.SUPER_ADMIN);
+//        user.setPassword(passwordEncoder.encode(user.getPassword()));
+//        userRepository.save(user);
+//        AuthenticationResponseDto authResponseDto = new ModelMapper().map(user, AuthenticationResponseDto.class);
+//
+//        try {
+//            String accessToken = jwtTokenProvider.generateToken(user);
+//            authResponseDto.setJwt(accessToken);
+//        } catch (Exception e) {
+//            throw new TokenGenerationException();
+//        }
+//        return authResponseDto;
+//    }
+//}
