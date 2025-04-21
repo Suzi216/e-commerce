d6eac1119307cf196538f06b607236221c6a88ec
Admire-Companies <admirecompanies7@gmail.com>
2025-04-21 18:00:44 +0200

    Remove security

--- src/main/java/com/projects/e_commerce_service/config/SecurityConfig.java
+++ src/main/java/com/projects/e_commerce_service/config/SecurityConfig.java
@@ -1,80 +1,80 @@
-package com.projects.e_commerce_service.config;
-
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
-import org.springframework.security.web.SecurityFilterChain;
-
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import static org.springframework.security.config.Customizer.withDefaults;
-
-//https://blog.devops.dev/spring-boot-3-spring-security-6-with-jwt-authentication-and-authorization-new-2024-989323f29b84
-
-@Configuration
-@EnableWebSecurity
-@EnableMethodSecurity
-
-@RequiredArgsConstructor
-public class SecurityConfig {
-    @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable()) // Disable CSRF for testing
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/authenticate/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // No sessions (for JWT or API tokens)
-                )
-//                .oauth2ResourceServer(oauth2 -> oauth2.jwt()); // Enable Basic Authentication (Optional)
-                .oauth2ResourceServer(oauth2 -> oauth2
-                .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))); // Apply JWT converter
-                // .httpBasic(); // Or just comment the entire oauth2ResourceServer line
-
-
-        return http.build();
-    }
-    @Bean
-    JwtAuthenticationConverter jwtAuthenticationConverter() {
-        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
-        grantedAuthoritiesConverter.setAuthoritiesClaimName("scope"); // Ensure correct claim name
-        grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_"); // Ensure roles have prefix
-
-        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
-        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
-            Collection<GrantedAuthority> authorities = grantedAuthoritiesConverter.convert(jwt);
-
-            // Convert roles in scope to Spring Security authorities
-            List<GrantedAuthority> roleAuthorities = jwt.getClaimAsStringList("scope").stream()
-                    .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
-                    .collect(Collectors.toList());
-
-            return Stream.concat(authorities.stream(), roleAuthorities.stream()).collect(Collectors.toList());
-        });
-
-        return jwtAuthenticationConverter;
-    }
-
-    @Bean
-    public PasswordEncoder encoder() {
-        return new BCryptPasswordEncoder(11);
-    }
-}
-
+//package com.projects.e_commerce_service.config;
+//
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+//import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//import java.util.Collection;
+//import java.util.List;
+//import java.util.stream.Collectors;
+//import java.util.stream.Stream;
+//
+//import static org.springframework.security.config.Customizer.withDefaults;
+//
+////https://blog.devops.dev/spring-boot-3-spring-security-6-with-jwt-authentication-and-authorization-new-2024-989323f29b84
+//
+//@Configuration
+//@EnableWebSecurity
+//@EnableMethodSecurity
+//
+//@RequiredArgsConstructor
+//public class SecurityConfig {
+//    @Bean
+//    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf(csrf -> csrf.disable()) // Disable CSRF for testing
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/api/authenticate/**").permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .sessionManagement(session -> session
+//                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // No sessions (for JWT or API tokens)
+//                )
+////                .oauth2ResourceServer(oauth2 -> oauth2.jwt()); // Enable Basic Authentication (Optional)
+//                .oauth2ResourceServer(oauth2 -> oauth2
+//                .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))); // Apply JWT converter
+//                // .httpBasic(); // Or just comment the entire oauth2ResourceServer line
+//
+//
+//        return http.build();
+//    }
+//    @Bean
+//    JwtAuthenticationConverter jwtAuthenticationConverter() {
+//        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+//        grantedAuthoritiesConverter.setAuthoritiesClaimName("scope"); // Ensure correct claim name
+//        grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_"); // Ensure roles have prefix
+//
+//        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+//        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
+//            Collection<GrantedAuthority> authorities = grantedAuthoritiesConverter.convert(jwt);
+//
+//            // Convert roles in scope to Spring Security authorities
+//            List<GrantedAuthority> roleAuthorities = jwt.getClaimAsStringList("scope").stream()
+//                    .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+//                    .collect(Collectors.toList());
+//
+//            return Stream.concat(authorities.stream(), roleAuthorities.stream()).collect(Collectors.toList());
+//        });
+//
+//        return jwtAuthenticationConverter;
+//    }
+//
+//    @Bean
+//    public PasswordEncoder encoder() {
+//        return new BCryptPasswordEncoder(11);
+//    }
+//}
+//
--- src/main/java/com/projects/e_commerce_service/services/JwtTokenProvider.java
+++ src/main/java/com/projects/e_commerce_service/services/JwtTokenProvider.java
@@ -1,66 +1,66 @@
-package com.projects.e_commerce_service.services;
-
-import com.projects.e_commerce_service.entities.User;
-import io.jsonwebtoken.SignatureAlgorithm;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import org.bouncycastle.util.io.pem.PemObject;
-import org.bouncycastle.util.io.pem.PemReader;
-import org.springframework.stereotype.Component;
-import java.io.File;
-import java.io.FileReader;
-import java.security.*;
-import io.jsonwebtoken.Jwts;
-import java.security.interfaces.RSAPrivateKey;
-import java.security.spec.PKCS8EncodedKeySpec;
-import java.util.Date;
-import java.util.List;
-
-@Component
-@Setter
-@Getter
-@RequiredArgsConstructor
-public class JwtTokenProvider {
-    private final String SECRET_KEY = "your_secret_key";
-    private final long EXPIRATION_TIME = 86400000; // 24 hours
-
-//    @Value("${custom.key-pair.private_key}")
-    File privateKeyFile= new File("C:/Users/suzan/Downloads/private_key.pem");
-
-    public String generateToken(User user) throws NoSuchAlgorithmException {
-        Date now = new Date();
-        Date expiration = new Date(now.getTime() + EXPIRATION_TIME);
-        RSAPrivateKey keyPair = getRSAPrivateKey();
-
-        return Jwts.builder()
-                .setSubject(user.getUsername())
-                .claim("scope", List.of(user.getRole().toString()))
-                .setIssuedAt(now)
-                .setExpiration(expiration)
-                .signWith(keyPair, SignatureAlgorithm.RS256)
-                .compact();
-    }
-
-    public RSAPrivateKey getRSAPrivateKey() {
-        KeyFactory factory = null;
-        try {
-            factory = KeyFactory.getInstance("RSA");
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
-
-        try (FileReader keyReader = new FileReader(privateKeyFile);
-             PemReader pemReader = new PemReader(keyReader)) {
-
-            PemObject pemObject = pemReader.readPemObject();
-            byte[] content = pemObject.getContent();
-            PKCS8EncodedKeySpec privKeySpec = new PKCS8EncodedKeySpec(content);
-            return (RSAPrivateKey) factory.generatePrivate(privKeySpec);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-}
-
+//package com.projects.e_commerce_service.services;
+//
+//import com.projects.e_commerce_service.entities.User;
+//import io.jsonwebtoken.SignatureAlgorithm;
+//import lombok.Getter;
+//import lombok.RequiredArgsConstructor;
+//import lombok.Setter;
+//import org.bouncycastle.util.io.pem.PemObject;
+//import org.bouncycastle.util.io.pem.PemReader;
+//import org.springframework.stereotype.Component;
+//import java.io.File;
+//import java.io.FileReader;
+//import java.security.*;
+//import io.jsonwebtoken.Jwts;
+//import java.security.interfaces.RSAPrivateKey;
+//import java.security.spec.PKCS8EncodedKeySpec;
+//import java.util.Date;
+//import java.util.List;
+//
+//@Component
+//@Setter
+//@Getter
+//@RequiredArgsConstructor
+//public class JwtTokenProvider {
+//    private final String SECRET_KEY = "your_secret_key";
+//    private final long EXPIRATION_TIME = 86400000; // 24 hours
+//
+////    @Value("${custom.key-pair.private_key}")
+//    File privateKeyFile= new File("C:/Users/suzan/Downloads/private_key.pem");
+//
+//    public String generateToken(User user) throws NoSuchAlgorithmException {
+//        Date now = new Date();
+//        Date expiration = new Date(now.getTime() + EXPIRATION_TIME);
+//        RSAPrivateKey keyPair = getRSAPrivateKey();
+//
+//        return Jwts.builder()
+//                .setSubject(user.getUsername())
+//                .claim("scope", List.of(user.getRole().toString()))
+//                .setIssuedAt(now)
+//                .setExpiration(expiration)
+//                .signWith(keyPair, SignatureAlgorithm.RS256)
+//                .compact();
+//    }
+//
+//    public RSAPrivateKey getRSAPrivateKey() {
+//        KeyFactory factory = null;
+//        try {
+//            factory = KeyFactory.getInstance("RSA");
+//        } catch (NoSuchAlgorithmException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        try (FileReader keyReader = new FileReader(privateKeyFile);
+//             PemReader pemReader = new PemReader(keyReader)) {
+//
+//            PemObject pemObject = pemReader.readPemObject();
+//            byte[] content = pemObject.getContent();
+//            PKCS8EncodedKeySpec privKeySpec = new PKCS8EncodedKeySpec(content);
+//            return (RSAPrivateKey) factory.generatePrivate(privKeySpec);
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//
+//}
+//
